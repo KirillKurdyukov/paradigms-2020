97db6fc0a852e7185ab0b3c740e5f42979ced5c0
Kirill <kurdyukov_kirya@mail.ru>
2020-04-23 22:06:09 +0300

    all tasks

--- clojure/expression.clj
+++ clojure/expression.clj
@@ -1,9 +1,22 @@
 (defn constant [value] (fn [m] value))
 (defn variable [varName] (fn [m] (get m varName)))
-(defn unaryOperation [f] (fn [one] (fn [m] (f (one m)))))
-(defn binaryOperation [f] (fn [first, second] (fn [m] (f (first m) (second m)))))
-(def negate (unaryOperation -))
-(def multiply (binaryOperation *))
-(def add (binaryOperation +))
-(def subtract (binaryOperation -))
-(def divide (binaryOperation (fn [a, b] (/ (double a) (double b)))))
+(defn operation [f] (fn [& args] (fn [m] (apply f (mapv (fn [x] (x m)) args)))))
+(def negate (operation -))
+(def multiply (operation *))
+(def add (operation +))
+(def subtract (operation -))
+(def divide (operation (fn [a, b] (/ (double a) (double b)))))
+(def isVariable (fn [symbol] (if (or (= symbol 'x) (= symbol 'y) (= symbol 'z)) true false)))
+(def mapOperation {
+                   "+"      add
+                   "-"      subtract
+                   "/"      divide
+                   "*"      multiply
+                   "negate" negate
+                   })
+(defn parse [expression] (cond (number? expression) (constant expression)
+                                 (isVariable expression) (variable (name expression))
+                               :else (apply (get mapOperation (name (first expression))) (mapv parse (pop expression)))))
+(defn parseFunction [string]
+  (let [expression (read-string string)]
+    (parse expression)))
