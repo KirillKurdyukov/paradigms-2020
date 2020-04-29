f09615ff7b355f576719f16b4537f19c34963232
Kirill <kurdyukov_kirya@mail.ru>
2020-04-29 12:54:49 +0300

    all tasks

--- clojure/expression.clj
+++ clojure/expression.clj
@@ -9,18 +9,17 @@
 (def subtract (operation -))
 (def divide (operation (fn [a, b] (/ (double a) (double b)))))
 (def isVariable (fn [symbol] (if (or (= symbol 'x) (= symbol 'y) (= symbol 'z)) true false)))
-(def mapOperation {
-                   "+"      add
-                   "-"      subtract
-                   "/"      divide
-                   "*"      multiply
-                   "negate" negate
-                   "exp"    exp
-                   "ln"     ln
-                   })
-(defn parse [expression] (cond (number? expression) (constant expression)
-                               (isVariable expression) (variable (name expression))
-                               :else (apply (get mapOperation (name (first expression))) (mapv parse (pop expression)))))
 (defn parseFunction [string]
-  (let [expression (read-string string)]
-    (parse expression)))
+  (letfn [(parse [expression]
+            (let [mapOperation {
+                                "+"      add
+                                "-"      subtract
+                                "/"      divide
+                                "*"      multiply
+                                "negate" negate
+                                "exp"    exp
+                                "ln"     ln
+                                }] (cond (number? expression) (constant expression)
+                                         (isVariable expression) (variable (name expression))
+                                         :else (apply (get mapOperation (name (first expression))) (mapv parse (pop expression))))))]
+    (parse (read-string string))))
