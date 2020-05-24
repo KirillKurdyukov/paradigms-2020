e83ec51b77f3aae8c97f507a8e38c927b49124ae
Kirill <kurdyukov_kirya@mail.ru>
2020-05-24 21:24:08 +0300

    prolog

--- prolog/primes.pl
+++ prolog/primes.pl
@@ -8,6 +8,32 @@ prime(N) :- N > 1, Y is 2, \+divisible(N, Y).
 %composite(N) - проверяет, что N - составное.
 composite(N) :- N > 1, \+prime(N).
 
+%next_prime(N, Result) - следующее простое.
+next_prime(N, A) :- inc(N, R), prime(R), !, A is R.
+next_prime(N, A) :- inc(N, R), \+prime(R), next_prime(R, M), A is M.
+
+%concat(A1, A2, R) - конкатинирует два массива.
+concat([], B, B).
+concat([H | T], B, [H | R]) :- concat(T, B, R).
+
+%divisors(N, D, R) - напихает в массив R делители числа N.
+divisors(1, _, []).
+divisors(N, D, R1) :- N >= D, 0 is mod(N, D), M is div(N, D), prime(D), divisors(M, D, R2), !, concat([D | _], R2, R1).
+divisors(N, D, R1) :- N >= D, \+(0 is mod(N, D)), next_prime(D, DN), divisors(N, DN, R1).
+
+%multiply_list([H | T], R) - вернет произведение элементов в массиве.
+multiply_list([], R) :- R is 1.
+multiply_list([H | T], R) :- multiply_list(T, R1), R is H * R1.
+
+%correct_sort_list([H | T], R)
+correct_sort_list([H], 1).
+correct_sort_list([H1, H2 | T], R) :- H1 =< H2, correct_sort_list([H2 | T], R).
+correct_sort_list([H1, H2 | T], R) :- H1 > H2, R is 0.
+
 %prime_divisors - проверяющее, что список Divisors содержит все простые делители числа N, упорядоченные по возрастанию. Если N делится на простое число P несколько раз, то Divisors должен содержать соответствующее число копий P.
-prime_divisors(N, []) :- N is 1.
-prime_divisors(N, [H | T]) :- 0 is mod(N, H), N1 is div(N, H), prime_divisors(N1, T).
+prime_divisors(N, R) :- integer(N), divisors(N, 2, R), !.
+prime_divisors(N, [H | T]) :- correct_sort_list([H | T], R), 1 is R, multiply_list([H | T], N1), N is N1, !.
+prime_divisors(N, []) :- N is 1, !.
+
+
+
