db4f5bf464ad17ef8bcce5da85ad6414b12bc8c4
Kirill <kurdyukov_kirya@mail.ru>
2020-06-02 22:13:21 +0300

    prolog 14 HARD

--- prolog/tree-map.pl
+++ prolog/tree-map.pl
@@ -19,3 +19,35 @@ split(node(Key, Val, Y, Left, Rigth), X, (NL, node(Key, Val, Y, NLeft, Rigth))) 
 	Key >= X, 
 	split(Left, X, (NL, NLeft)),
 	!. 
+map_get(TreeMap, Key, Value) :-
+	split(TreeMap, Key, (L, R)),
+	NKey is 1 + Key,
+	split(R, NKey, (NL, NR)),
+	NL \= null,
+	split(R, NKey, (node(Key1,Value1, Y, null, null), NR)),
+	Value1 is Value, !.
+
+map_Key(TreeMap, Key, Value) :-
+	split(TreeMap, Key, (L, R)),
+	NKey is 1 + Key,
+	split(R, NKey, (NL, NR)),
+	NL \= null.
+
+map_put(TreeMap, Key, Value, Result) :-
+	map_Key(TreeMap, Key, Value1),
+	split(TreeMap, Key, (L, R)),
+	split(R, Key + 1, (NL, NR)),
+	rand_int(1000000, Y),
+	merge(L, node(Key, Value, Y, null, null), Left),
+	merge(Left, NR, Result).
+
+map_remove(TreeMap, Key, Result)	:-
+	split(TreeMap, Key, (L, R)),
+	split(R, Key + 1, (NL, NR)),
+	merge(L, NR, Result).
+
+map_build([(Key, Val)], TreeMap) :-
+	map_put(null, Key, Value, TreeMap), !.
+map_build([(Key, Val) | T], TreeMap) :-
+	map_build(T, NTreeMap),
+	map_put(NTreeMap, Key, Value, TreeMap), !.
